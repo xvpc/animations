be3feda2d515a87eee05bcf1d45b3fe37c7c8505
Viper <viperxx100@gmail.com>
2024-05-27 11:14:50 +0300

    #

--- utils/ImageHolder.tsx
+++ utils/ImageHolder.tsx
@@ -10,17 +10,33 @@ type ImageProps = {
 }
 
 export default function ImageHolder({ title, url, name, width, height, slider }: ImageProps ) {
-    return (
-        <div className=''>
-            <img 
-            onError={(e) => {
-                e.currentTarget.src = "." + "/images/placeholder.png";
-                e.currentTarget.onerror = null;
-            }}
-            src={"." + url} alt={title}
-            style={{aspectRatio: "5/6", minWidth: slider ? "auto" : " 300px", maxWidth: slider ? "auto" : "440px"}}
-            className={`p-0 m-auto object-cover`}
-            />
-        </div>
-    );
+    if(process.env.NODE_ENV != "production"){
+        return (
+            <div className=''>
+                <img 
+                onError={(e) => {
+                    e.currentTarget.src = "./images/placeholder.png";
+                    e.currentTarget.onerror = null;
+                }}
+                src={"." + url} alt={title}
+                style={{aspectRatio: "5/6", minWidth: slider ? "auto" : " 300px", maxWidth: slider ? "auto" : "440px"}}
+                className={`p-0 m-auto object-cover`}
+                />
+            </div>
+        );
+    }else{
+        return (
+            <div className=''>
+                <img 
+                onError={(e) => {
+                    e.currentTarget.src = "/images/placeholder.png";
+                    e.currentTarget.onerror = null;
+                }}
+                src={url} alt={title}
+                style={{aspectRatio: "5/6", minWidth: slider ? "auto" : " 300px", maxWidth: slider ? "auto" : "440px"}}
+                className={`p-0 m-auto object-cover`}
+                />
+            </div>
+        );
+    }
 }
