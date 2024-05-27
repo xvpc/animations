3a58feaf3fe74c59f047901016cb8a37a3b8bc63
Viper <viperxx100@gmail.com>
2024-05-27 10:59:41 +0300

    #

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["i.ibb.co"]
+  },
+  output: 'export',
+  distDir: 'out',
+  basePath: '/typer'
 };
 
 export default nextConfig;
